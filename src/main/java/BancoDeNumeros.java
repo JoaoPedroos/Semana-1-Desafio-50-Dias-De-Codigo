986763791ef7c754f09946a4bfa8f000d506bada
Joao <joaopedroenv@gmail.com>
2026-01-20 00:47:48 -0300

    feat: busca de numeros com scanner, e soma, média dos numeros informados e maior e menor numero.

---
+++ src/main/java/BancoDeNumeros.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class BancoDeNumeros {
+    public static void main(String[] args){
+
+
+        int i;
+        double media = 0;
+        double soma = 0;
+        Double verfMaior = 0.0;
+        Double verfMenor = 1.7976931348623157E308;
+        Scanner teclado = new Scanner(System.in);
+        List<Double> listaNumeros = new ArrayList<>();
+
+
+        System.out.println("");
+        System.out.println("Você deseja guardar quantos numeros?");
+        int n = teclado.nextInt();
+
+
+        for(i = 1; i <= n; i++){
+            System.out.println("Digite o "+ i +"° numero.");
+            Double valores = teclado.nextDouble();
+
+            if(valores.equals("") || valores == null){
+                System.out.println("O valor inserido é inválido");
+            }
+            else{
+                listaNumeros.add(valores);
+                soma = soma + valores ;
+
+                if(valores > verfMaior){
+                    verfMaior = valores;
+                } else{
+                    System.out.println("");
+                }
+                if (valores < verfMenor) {
+                    verfMenor = valores;
+                }
+                else{
+                    System.out.println("");
+                }
+            }
+
+        }
+
+        media = soma / n;
+
+        System.out.println("Média: "+media);
+        System.out.println("Soma: "+soma);
+        System.out.println("Maior N°:  BREVE");
+        System.out.println("Menor N°:  ");
+
+    }
+
+}
